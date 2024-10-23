abc8f4eafed19bb3e58df84a6fa50f9e9ec0e6fc
mrtaz77 <mrtazwar23@gmail.com>
2024-10-23 15:18:23 +0600

    patch gen for offline3

--- patch_generator.sh
+++ patch_generator.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-patch_file_name="B1_soln"
-output_dir="../Onlines/SystemCall/B1/"
+patch_file_name="scheduling"
+output_dir="../Offlines/Scheduling"
 
 cd xv6-riscv || {
 	echo "xv6-riscv dir not found!"
 	exit 1
 }
 
-current_commit=$(git rev-parse HEAD)
-
-origin_head=$(git rev-parse origin/riscv)
-
-if [ "$current_commit" == "$origin_head" ]; then
-	echo "The submodule is already up-to-date. No patch needed."
-	exit 0
-fi
-
 patch_file="${patch_file_name}.patch"
-git diff "$origin_head" "$current_commit" > "$patch_file"
+
+git add --all
+git diff HEAD > "$patch_file"
 
 if [ -s "$patch_file" ]; then
 	echo "Patch file generated successfully: $patch_file"
